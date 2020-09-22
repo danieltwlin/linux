aa78e6d456ccc7a56cc9af41b3337d2c764331a7
daniel <67772752+danieltwlin@users.noreply.github.com>
2020-09-22 15:35:01 +0800

    Update fio_demo.sh

--- performance/fio_demo.sh
+++ performance/fio_demo.sh
@@ -3,6 +3,8 @@
 #隨機寫入測試
 fio -directory=/data/ -name=mytest -direct=1 -rw=randwrite -bs=4k -size=1M -numjobs=16 -time_based -runtime=100 -group_reporting
 
+fio -directory=/mnt/temp/ -name=mytest -direct=1 -rw=randwrite -bs=4k -size=1G -numjobs=3 -time_based -runtime=100 -group_reporting
+
 #directory: 设置filename的路径前缀。在后面的基准测试中，采用这种方式来指定设备。
 
 #順序讀寫測試
