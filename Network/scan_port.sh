30c3f086d854d51dea69b61bd716799098ece7d7
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-03-09 14:47:01 +0800

    Update scan_port.sh

--- Network/scan_port.sh
+++ Network/scan_port.sh
@@ -3,6 +3,9 @@
 
  nmap -p 80 google.com
 
+# 用法
+# nmap [Scan Types掃瞄類型] [Options掃瞄參數] {target specification指定目標}
+
 # STATE : OPEN ( Port 有啟用，且打開 )
 #               CLOSE ( Port 有啟用，但目前關閉)
 #               Filtered  ( Port 未啟用 )
