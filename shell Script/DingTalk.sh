164aae291b90e2ca5456e8c0ecc1235b807e74c8
daniel <67772752+danieltwlin@users.noreply.github.com>
2021-04-13 16:55:26 +0800

    Create DingTalk.sh

---
+++ shell Script/DingTalk.sh
@@ -0,0 +1,9 @@
+curl 'https://oapi.dingtalk.com/robot/send?access_token=xxxxxxxx' \
+   -H 'Content-Type: application/json' \
+   -d '
+  {"msgtype": "text",
+    "text": {
+        "content": "How are you"
+     }
+
+  }'
